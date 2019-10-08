70700e9d167305510a580ebacb88cff2867d737c
Sloane <sloanehawkins@gmail.com>
2019-10-08 11:55:59 -0700

    Add basic javascript files

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,23 @@
+
+var A = "Coding is fun ";
+document.write(A)
+
+var B = "Alert string "
+
+window.alert(B);
+
+document.write("Sadie says, \"Hello\" ");
+
+document.write("Roses are red" + " violets are blue")
+
+var C= "this is a poem" + " but not a haiku"
+document.write(C)
+
+var dog1 = "Sadie", dog2 = "Bella", dog3 = "Waylon"
+var dog1 = dog1.fontcolor("blue");
+var dog2 = dog2.fontcolor("pink");
+var dog3 = dog3.fontcolor("gray")
+
+document.write(dog3)
+
+document.write(3*4)
