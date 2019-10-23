c6042eda3ee110382e2403872b78268e17a84af5
Sloane <sloanehawkins@gmail.com>
2019-10-23 10:44:31 -0700

    add portfolio website folder

---
+++ Portfolio Website/JS/Portfolio.js
@@ -0,0 +1,10 @@
+
+// These functions open and close the contact form 
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    // this.close();
+}
